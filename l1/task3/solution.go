03812f4c1610258780385043b73c42b79a73680c
rel4t <volckovle5h@yandex.ru>
2025-09-05 21:08:21 +0300

    Added multi-worker solution for concurrent channel data processing

---
+++ l1/task3/solution.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strconv"
+	"time"
+)
+
+func worker(id int, jobs <-chan int) {
+	for job := range jobs {
+		fmt.Printf("Работник %d делает %d\n", id, job)
+	}
+}
+
+func main() {
+	if len(os.Args) < 2 {
+		fmt.Printf("Нет данных\n")
+		return
+	}
+
+	n, err := strconv.Atoi(os.Args[1])
+	if err != nil || n <= 0 {
+		fmt.Printf("Ошибка при чтении или некорректное количество работников")
+	}
+
+	jobs := make(chan int)
+
+	for i := 1; i <= n; i++ {
+		go worker(i, jobs)
+	}
+
+	count := 1
+
+	for {
+		jobs <- count
+		count++
+		time.Sleep(100 * time.Millisecond)
+	}
+}
